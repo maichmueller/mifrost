beacfe664f842d78d60d6e9fa3bd4eb069b1c93b
Michael Aichmueller <m.aichmueller@gmail.com>
2026-01-25 16:21:28 +0100

    add phmap specializations for dependencies (loki)

---
+++ src/mifrost/gtl/phmap.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <parallel_hashmap/phmap.h>
+
+namespace gtl {
+
+using phmap::flat_hash_map;
+using phmap::flat_hash_set;
+using phmap::parallel_flat_hash_map;
+using phmap::parallel_flat_hash_set;
+
+} // namespace gtl
